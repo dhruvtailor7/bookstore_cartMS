210f63b086f99ed9faf7c07f43631f574d44bbd2
dhruvtailor7 <32268377+dhruvtailor7@users.noreply.github.com>
2020-03-24 12:43:28 +0530

    Update CartMsApplication.java

--- src/main/java/com/coviam/bookstore/cartMS/CartMsApplication.java
+++ src/main/java/com/coviam/bookstore/cartMS/CartMsApplication.java
@@ -4,12 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableEurekaClient
-@EnableSwagger2WebMvc
 public class CartMsApplication {
 
 	public static void main(String[] args) {
