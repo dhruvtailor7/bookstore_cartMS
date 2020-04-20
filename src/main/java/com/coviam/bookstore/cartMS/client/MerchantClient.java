96ded5642b28c8e32d77b8c807c12e72d4766a09
dhruvtailor7 <32268377+dhruvtailor7@users.noreply.github.com>
2020-04-20 18:49:33 +0530

    Update MerchantClient.java

--- src/main/java/com/coviam/bookstore/cartMS/client/MerchantClient.java
+++ src/main/java/com/coviam/bookstore/cartMS/client/MerchantClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient("merchant")
+@FeignClient("http://bookstore-merchant.herokuapp.com/")
 public interface MerchantClient {
     @PostMapping("/getQuantity")
     public String getQuantity(@RequestBody QuantityDTO quantityDTO);
